7d10ac3e289e97e7a48d78c2e5582759e7223520
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-10-20 15:35:54 +0600

    Add updating cache values

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 /* global it */
 
-var after = require('../index.js');
+var after   = require('../index.js');
 var events  = require('events');
 var ee      = new events.EventEmitter();
+var assert  = require('assert');
 
 it('should call callback on event once', function (done) {
     after(ee, 'bang', done);
@@ -14,4 +15,15 @@ it('should call callback after event', function (done) {
     after(ee, 'bang');
     ee.emit('bang');
     after(ee, 'bang', done);
+});
+
+it('should update cached values after event', function (done) {
+    after(ee, 'bang');
+    ee.emit('bang', 'wow');
+    ee.emit('bang', 'wow!');
+
+    after(ee, 'bang', function (data) {
+        assert.equal(data, 'wow!')
+        done();
+    });
 });
